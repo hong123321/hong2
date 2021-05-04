99c8304f64df9dc480567ad234a7f41a5e3c8b22
unknown <lexuanhong913@gmail.com>
2021-05-04 21:18:30 +0700

    first commit

---
+++ frontend/congnghe.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link href="../css/congnghe.css" rel="stylesheet" type="text/css" /> 
+</head>
+
+<body>
+<div id="contaner">
+	<div id="header">
+		<div id="congcu">
+			<div id="logo"><a href=""><img style=" 	width: 60%;" src="../images/Logo.png"></a></div>
+			<div id="giua">
+			    <div class="congcu1"><p>Hotline:0929777727</p></div>
+			    <div class="congcu1"><p>Email:info@nobicode.net</p></div>
+			    <div class="congcu1"><p>Open:8h-17h</p></div>
+			</div>
+			<div id="mxh">
+			   <div  class="mxh1"><a href="#"><img src="../images/icon Facebook.png"  style="height: 50px; width: 50px;"></a></div>
+			   <div  class="mxh1"><a href="#"><img src="../images/icon Youtube.png"   style="height: 50px; width: 50px;"></a></div>
+			   <div  class="mxh1"><a href="#"><img src="../images/icon Instagram.png" style="height: 50px; width: 50px;"></a></div>
+			   <div  class="mxh1"><a href="#"><img src="../images/icon Piscart.png"   style="height: 50px; width: 50px;"></a></div>
+			</div>
+		</div>
+		<div class="b">
+			<ul id="main-manu">
+                   <li><a href="#">HOME</a></li>
+                   <li><a href="#">CÔNG NGHỆ</a></li>
+                   <li><a href="#">GIÁO DỤC</a></li>
+                   <li><a href="#">TIN TỨC</a></li>
+                   <li><a href="#">LIÊN HỆ</a></li>
+                </ul>
+		</div>
+	</div>
+	<div id="banner" >
+		<div class="a">
+			<img src="../images/banner.jpg" style="width: 100%; height:100%"> 
+			
+		</div>
+		
+		
+		
+	</div>
+	<div id="c">
+			
+                  <div class="img1"><a href="#"><img src="../images/1.png" style="height:60%; width:60%;"></a><p>CÔNG NGHỆ</p></div>
+                  <div class="img1"><a href="#"><img  src="../images/2.png" style="height:60%; width:60%;"></a><p>GIÁO DỤC</p></div>
+                  <div class="img1"><a href="#"><img src="../images/3.png" style="height:60%; width:60%;"></a><p>THƯƠNG MẠI</p></div>
+                  <div class="img1"><a href="#"><img src="../images/4.png" style="height:60%; width:60%;"></a><p>DỊCH VỤ</p></div>
+                  
+                
+			</div>
+	
+	<div id="body2" class="col">
+	   <div class="tintuc"><h2>Tin tức và bài viết mới</h2></div>
+		<div id="1"  class="row"><img src="../images/th.jpg" style="height: 400px; width:400px;">
+		<h3>những bô não nhân tạo</h3>
+			<p>giới trẻ mỹ đang sử dụng AI để thay đổi nhiều ngành nghề, song ở Việt Nam nhiều bạn trẻ vẫn nghĩ AI là khoa học viễn tưởng</p>
+			<button class="button">Xem tiếp>>>...</button>
+		</div>
+		<div id="2"  class="row"><img src="../images/th.jpg" style="height: 400px; width:400px;">
+		<h3>AI thay đổi nền kinh tế chúng ta như thế nào?</h3>
+			<p>Trên thớ giớ nhiều chính phủ đã cam kết về việc đầu tư cụ thẻ về AI,bộ quốc phòng MỸ tuyên bố đầu tư vào 2 tỷ USD trong năm năm tới cho ngành này. Trung Quốc đưa ra "chiến lược quốc gia AI" với cam kết hàng chục ty USD...</p>
+			<button class="button">Xem tiếp>>>...</button>
+		</div>
+		<div id="3"  class="row"><img src="../images/th.jpg" style="height: 400px; width:400px;">
+		<h3>Trước mỗi cải tiến tự động hóa ở phạm vi lớn, luôn có người lo lắng vè tương lại không?</h3>
+			<p>Những tương lai luôn có việc để làm, chỉ là yêu cầu và bản chất công việc khác đi.Tự động hóa khiến nhiều công việc trở nên lạc hậu, nhưng nó cũng khiến nhiều ngành nghề trở nên dễ dàng hơn,nâng cao chất lượng cuộc sống con người</p>
+			<button class="button">Xem tiếp>>>...</button>
+		</div>
+	</div>
+	<div class="hoatdong">
+	<h2 style="text-align: center; color: #1475CF;">HOẠT ĐỘNG VỀ NÔNG NGHỆ</h2>
+		<div class="congnghetrai">
+		<div class="trai1">
+			<div class="hinh">
+			<img src="../images/4.0.jpg" style="height: 250px;width:80%;">
+				</div>
+			<div class="chu"><h3>Đánh giá vai trò của khoa học công nghiệp đối với nền kinh tế của VIỆT NAM</h3>
+			<p>Tác giả:nobicode.net</p>
+			<p style="padding-top: 0px;">đối với tăng trưởng kinh tế luôn là vấn đề hàng đầu đối với mỗi quốc gia trên thế giới, trong đó có cả Việt Nam, kinh tế tăng trưởng tạo tiền đề tăng trưởng tạo tiền đề phát triển đất nước về mọi mặt như: Tăng vốn tích lũy đầu tư mở rộng sản xuaasrt kinh doanh;nâng cao đời sống cải thiện người dân;cai thiện các vấn đề phúc lợi công cộng:văn hóa giáo dục y tế và xóa đói giảm nghèo</br>Việc xác định các nhân tốc tác động nói chung,vai trò khoa học-công nghệ nói riêng đến tăng trưởng kinh tế</p>
+			</div>
+			</div>
+		<div class="trai2">
+			<div class="hinh"><img src="../images/4.0.jpg" style="height: 250px;width:100%;"></div>
+			<div class="chu"><h3>Đánh giá vai trò của khoa học công nghiệp đối với nền kinh tế của VIỆT NAM</h3>
+			<p>Tác giả:nobicode.net</p>
+			<p style="padding-top: 0px;">đối với tăng trưởng kinh tế luôn là vấn đề hàng đầu đối với mỗi quốc gia trên thế giới, trong đó có cả Việt Nam, kinh tế tăng trưởng tạo tiền đề tăng trưởng tạo tiền đề phát triển đất nước về mọi mặt như: Tăng vốn tích lũy đầu tư mở rộng sản xuaasrt kinh doanh;nâng cao đời sống cải thiện người dân;cai thiện các vấn đề phúc lợi công cộng:văn hóa giáo dục y tế và xóa đói giảm nghèo</br>Việc xác định các nhân tốc tác động nói chung,vai trò khoa học-công nghệ nói riêng đến tăng trưởng kinh tế</p></div>
+			</div>
+		<div class="trai3">
+			<div class="hinh"><img src="../images/4.0.jpg" style="height: 250px;width:80%;"></div>
+			<div class="chu"><h3>Đánh giá vai trò của khoa học công nghiệp đối với nền kinh tế của VIỆT NAM</h3>
+			<p>Tác giả:nobicode.net</p>
+			<p style="padding-top: 0px;">đối với tăng trưởng kinh tế luôn là vấn đề hàng đầu đối với mỗi quốc gia trên thế giới, trong đó có cả Việt Nam, kinh tế tăng trưởng tạo tiền đề tăng trưởng tạo tiền đề phát triển đất nước về mọi mặt như: Tăng vốn tích lũy đầu tư mở rộng sản xuaasrt kinh doanh;nâng cao đời sống cải thiện người dân;cai thiện các vấn đề phúc lợi công cộng:văn hóa giáo dục y tế và xóa đói giảm nghèo</br>Việc xác định các nhân tốc tác động nói chung,vai trò khoa học-công nghệ nói riêng đến tăng trưởng kinh tế</p></div>
+			</div>
+		</div>
+		<div class="congnghephai">
+		<div class="phai1"><img src="../images/4.0.jpg" style="height: 250px;width:100%;"></div>
+		<div class="phai2"><img src="../images/4.0.jpg" style="height: 250px;width:100%;"></div>
+		</div>
+	</div>
+<div class="sanphamcongnghe">
+	<h1 style="text-align: center; color: #1475CF;">Sản phẩm công nghệ</h1>
+	<div class="">
+	</div>
+<div class="body2">
+		   <div class="body21">
+			   <h1 style="align-content: center; margin-left: 600px; color: #27B1E6; padding-top: 10%;"> Đối tác của NOBICOE</h1>
+		   </div>
+		   <div class="body22">
+			   <div><a href="https://www.facebook.com/xaoytuong">
+				   <img width=100% height="100%" src="img/xaoytuong.jpg">
+				   <h1> Xào Ý Tưởng </h1>
+				   </a>
+			   </div>
+			   <div><a href="https://www.mbageas.life/">
+				   <img width=100% height="100%" src="img/mb.jpg">
+				   <h1> MB ageas </h1>
+				   </a>
+			   </div>
+			   <div><a href="http://www.longhaigroup.com.vn/">
+				   <img width=100% height="100%" src="img/long.png">
+				   <h1> Long Hải Group </h1>
+				   </a>
+			   </div>
+		   </div>
+	   </div>
+<div class="chucnang">
+	<h2 align="center">Khám phá mọi chức năng của NOBICODE</h2>
+	
+         <div class="chucnang1">
+	     <div class="chucnang2"><a href="#"><img class="d" src="../images/a.png" style="height:120px; width:120px;"></a><p>CÔNG NGHỆ</p></div>
+         <div class="chucnang2"><a href="#"><img class="d" src="../images/b.png"  style="height:120px; width:120px;"></a><p>GIÁO DỤC</p></div>
+         <div class="chucnang2"><a href="#"><img class="d" src="../images/c.png"   style="height:120px; width:120px;"></a><p>THƯƠNG MẠI</p></div>
+         <div class="chucnang2"><a href="#"><img class="d" src="../images/d.png"   style="height:120px; width:120px;"></a><p>DỊCH VỤ</p>
+			 </div>
+	</div>
+	</div>
+	
+	<div class="footer">
+		
+	<div class="trai">
+		<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!
+    1d3723.8977453149246!2d105.83245751424809!3d21.036777085994046!
+    2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!
+    1s0x3135aba15ec15d17%3A0x620e85c2cfe14d4c!2zTMSDbmcgSOG7kyBDaMOtIE1pbmg!
+    5e0!3m2!1svi!2sus!4v1501056274257" width="400" height="300" 
+    frameborder="0" style="border:0" allowfullscreen></iframe></div></div>
+		
+<div class="phai"> 
+<h1 style="color: aqua;">NOBICODE EDUCATION&TECHNOLOGY</h1>
+
+<p>Chi nhánh: 174/33/23 Nguyễn Tư Giản, P.12, Quận Gò Vấp, TP. Hồ Chí Minh</p>
+<p>(+84) 929.7777.27</p>
+<p>info@nobicode.net</p>
+<p> www.nobicode.net</p>
+<p>Copyright © 2020 NobiCode - Designed by LXH</p></div>
+	</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--pc-->
+<link href="css/style.css" rel="stylesheet" type="text/css"> 
+<!--mobile-->
+<link href="css/viewport.css" rel="stylesheet" style="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+<script language="javascript" src="js/nut.js"></script>
+<script type="text/javascript" src="js/nut.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="contaner">
+	<div class="header">
+		<div id="congcu">
+			<div id="logo"><a href=""><img style=" 	width: 60%;" src="images/Logo.png"></a></div>
+			<div id="giua">
+			    <div class="congcu1"><p style="font-size:1vw">Hotline:0929777727</p></div>
+			    <div class="congcu1"><p style="font-size:1vw">Email:info@nobicode.net</p></div>
+			    <div class="congcu1"><p style="font-size:1vw">Open:8h-17h</p></div>
+			</div>
+			<div id="mxh">
+			   <div  class="mxh1"><a href="#"><img src="images/icon Facebook.png"  style="height: 50px; width: 50px;"></a></div>
+			   <div  class="mxh1"><a href="#"><img src="images/icon Youtube.png"   style="height: 50px; width: 50px;"></a></div>
+			   <div  class="mxh1"><a href="#"><img src="images/icon Instagram.png" style="height: 50px; width: 50px;"></a></div>
+			   <div  class="mxh1"><a href="#"><img src="images/icon Piscart.png"   style="height: 50px; width: 50px;"></a></div>
+			</div>
+		</div>
+		<nav>
+			  <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+			<ul>     
+                    <li><a href="action">HOME</a> </li>
+                    <li><a href="#">CÔNG NGHỆ</a></li>
+                    <li><a href="#">GIÁO DỤC</a></li>
+				    <li><a href="#">SẢN PHẨM</a></li>
+                    <li><a href="#">TIN TỨC</a></li>
+                    <li><a href="#">LIÊN HỆ</a></li>
+			
+              </ul>  
+		</nav>
+
+	</div>
+	<div id="banner">
+			<img src="images/banner.jpg"> 
+	</div>
+	<div id="c">
+			
+                  <div class="img1"><a href="frontend/congnghe.php"><img src="images/1.png"  style="max-width:60%; width:60%;"></a><p style="font-size:1vw">CÔNG NGHỆ</p></div>
+                  <div class="img1"><a href="#"><img  src="images/2.png" style="height:60%; width:60%;"></a><p style="font-size:1vw">GIÁO DỤC</p></div>
+                  <div class="img1"><a href="#"><img src="images/3.png" style="height:60%; width:60%;"></a><p style="font-size:1vw">THƯƠNG MẠI</p></div>
+                  <div class="img1"><a href="#"><img src="images/4.png" style="height:60%; width:60%;"></a><p style="font-size:1vw">DỊCH VỤ</p></div>
+                  
+                
+			</div>
+		
+	<div id="body">
+		<div id="trai">
+			<div id="trong1">
+			
+			<h2 style="font-size:2vw" class="tren">we are NOBICODE Educacation & Technology</h2>
+				<p  style="font-size:1vw" >Cong ty TNHH giáo dục và công nghệ NOBICODE với đội ngũ nhân sự trẻ năng động ,nhiệt huyết, nhạy bén, chuyên nghiệp được đào tạo bài bản và có tổ chức công ty khoa học</p>
+				
+			    <p style="font-size:1vw">Với trang thiết bị hiện đại , cùng đội ngũ kỹ thuật lành nghề và niềm đam mê công nghệ tột bậc , NOBICODE luôn ưu tiên áp dụng ng nghệ 4.0 vào lĩnh vực giáo dục , cuộc sống nhằm mang đến  những sản phấn có chất lượng cao , giúp hiện đại hóa , tiện ích hóa mọi công việc cho con người thông qua các dịch vụ , sản phẩm như: </p>
+				<p style="font-size:1vw">Với sự chuyên môn hóa từng bộ phận, tính trách nhiệm cao cùng những giám sát kỹ thuật làm việc nghiêm túc,cẩn thận, NOBICODE nổ lực tạo nên những sản phẩm hoàn thiện đến từng chi tiết nhỏ nhất với giá thành phù hợp </p>
+				<p style="font-size:1vw">TẦM NHÌN SỨ MỆNH</p>
+				<p style="font-size:1vw">Với phương châm"GIẢI PHÁP TIÊN TIẾN CỐNG HIẾN CUỘC SỐNG"và định hướng"LIÊN TỤC CẢI TIẾN"NOBICODE đã luôn nỗ lực về nhân lực ,vật lực,xây dựng uy tín thương hiệu,niềm tin với khách hàng từ những sản phẩm công ty cung cấp</p>
+				<H1 style="font-size:2vw" align="center">HÌNH ẢNH NHÂN SỰ</H1>
+		
+				</div>
+	  </div>
+		<div id="phai">
+			<div id="trong2">
+				<div class="hinhphai">
+					<img src="images/th.jpg" style="height: 100%; width:100%;">
+					</div>
+			</div>
+			</div>
+	</div>
+	
+	<div id="body2" class="col">
+	   <div class="tintuc"><h2 style="font-size:2vw">Tin tức và bài viết mới</h2></div>
+		<div id="1"  class="row"><img src="images/th.jpg" style="height: 400px; width:400px;">
+		<h3 style="font-size:1.5vw">những bô não nhân tạo</h3>
+			<p style="font-size:1vw">giới trẻ mỹ đang sử dụng AI để thay đổi nhiều ngành nghề, song ở Việt Nam nhiều bạn trẻ vẫn nghĩ AI là khoa học viễn tưởng</p>
+			<button style="font-size:1vw" class="button">Xem tiếp>>>...</button>
+		</div>
+		<div id="2"  class="row"><img src="images/th.jpg" style="height: 400px; width:400px;">
+		<h3 style="font-size:1.5vw">AI thay đổi nền kinh tế chúng ta như thế nào?</h3>
+			<p style="font-size:1vw">Trên thớ giớ nhiều chính phủ đã cam kết về việc đầu tư cụ thẻ về AI,bộ quốc phòng MỸ tuyên bố đầu tư vào 2 tỷ USD trong năm năm tới cho ngành này. Trung Quốc đưa ra "chiến lược quốc gia AI" với cam kết hàng chục ty USD...</p>
+			<button style="font-size:1vw" class="button">Xem tiếp>>>...</button>
+		</div>
+		<div id="3"  class="row"><img src="images/th.jpg" style="height: 400px; width:400px;">
+		<h3 style="font-size:1.5vw">Trước mỗi cải tiến tự động hóa ở phạm vi lớn, luôn có người lo lắng vè tương lại không?</h3>
+			<p style="font-size:1vw">Những tương lai luôn có việc để làm, chỉ là yêu cầu và bản chất công việc khác đi.Tự động hóa khiến nhiều công việc trở nên lạc hậu, nhưng nó cũng khiến nhiều ngành nghề trở nên dễ dàng hơn,nâng cao chất lượng cuộc sống con người</p>
+			<button style="font-size:1vw" class="button">Xem tiếp>>>...</button>
+		</div>
+	</div>
+	<div class="duan">
+		<div class="tren">
+	<div class="giaoduc"><img class="hinh" src="images/congnghe.jpg" style="height:500px; width:500px;"></div>
+	<div class="thuongmai"><img class="hinh" src="images/congnghe.jpg" style="height:500px; width:500px;"></div>
+			</div>
+		<div class="duoi">
+	<div class="congnge"><img class="hinh" src="images/congnghe.jpg" style="height:100%; width:500px;"></div>
+	<div class="dichvu"><img class="hinh" src="images/congnghe.jpg" style="height:100%; width:500px;"></div>
+			</div>
+			
+	</div>
+<div class="body2">
+		   <div class="body21">
+			   <h1 style="align-content: center; margin-left: 600px; color: #27B1E6; padding-top: 10%; font-size:2vw;"> Đối tác của NOBICOE</h1>
+		   </div>
+		   <div class="body22">
+			   <div><a href="https://www.facebook.com/xaoytuong">
+				   <img width=100% height="100%" src="img/xaoytuong.jpg">
+				   <h1 style="font-size:2vw"> Xào Ý Tưởng </h1>
+				   </a>
+			   </div>
+			   <div><a href="https://www.mbageas.life/">
+				   <img width=100% height="100%" src="img/mb.jpg">
+				   <h1  style="font-size:2vw"> MB ageas </h1>
+				   </a>
+			   </div>
+			   <div><a href="http://www.longhaigroup.com.vn/">
+				   <img width=100% height="100%" src="img/long.png">
+				   <h1  style="font-size:2vw"> Long Hải Group </h1>
+				   </a>
+			   </div>
+		   </div>
+	   </div>
+<div class="chucnang">
+	<h2 align="center"  style="font-size:1vw">Khám phá mọi chức năng của NOBICODE</h2>
+	
+         <div class="chucnang1">
+	     <div class="chucnang2"><a href="#"><img class="d" src="images/a.png" style="height:120px; width:120px;"></a><p style="font-size:1vw">CÔNG NGHỆ</p></div>
+         <div class="chucnang2"><a href="#"><img class="d" src="images/b.png"  style="height:120px; width:120px;"></a><p style="font-size:1vw">GIÁO DỤC</p></div>
+         <div class="chucnang2"><a href="#"><img class="d" src="images/c.png"   style="height:120px; width:120px;"></a><p style="font-size:1vw">THƯƠNG MẠI</p></div>
+         <div class="chucnang2"><a href="#"><img class="d" src="images/d.png"   style="height:120px; width:120px;"></a><p style="font-size:1vw">DỊCH VỤ</p>
+			 </div>
+	</div>
+	</div>
+	
+	<div class="footer">
+		
+	<div class="trai">
+		<div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!
+    1d3723.8977453149246!2d105.83245751424809!3d21.036777085994046!
+    2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!
+    1s0x3135aba15ec15d17%3A0x620e85c2cfe14d4c!2zTMSDbmcgSOG7kyBDaMOtIE1pbmg!
+    5e0!3m2!1svi!2sus!4v1501056274257" width="400" height="300" 
+    frameborder="0" style="border:0" allowfullscreen></iframe></div></div>
+		
+<div class="phai"> 
+<h1 style="color: aqua; font-size:2vw;">NOBICODE EDUCATION&TECHNOLOGY</h1>
+
+<p style="font-size:1vw">Chi nhánh: 174/33/23 Nguyễn Tư Giản, P.12, Quận Gò Vấp, TP. Hồ Chí Minh</p>
+<p style="font-size:1vw">(+84) 929.7777.27</p>
+<p style="font-size:1vw">info@nobicode.net</p>
+<p style="font-size:1vw"> www.nobicode.net</p>
+<p style="font-size:1vw">Copyright © 2020 NobiCode - Designed by LXH</p></div>
+	</div>
+</div>
+</body>
+</html>
